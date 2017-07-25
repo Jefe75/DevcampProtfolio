a9dd396c783092ad095aaafe002299a7695df155
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-07-25 16:50:07 +0000

    Integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,15 +6,17 @@ class PortfoliosController < ApplicationController
     def new
         @portfolio_item = Portfolio.new
     end
+    
     def create
         @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
         respond_to do |format|
             if @portfolio_item.save
-                format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+                format.html { redirect_to portfolios_path, notice: 'Your portfolio itme is now live.' }
             else
                 format.html { render :new }
             end
         end
     end
+    
 end
